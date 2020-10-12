ab661b1b1c579c9f5cfb884ae2bac10722a7a2b3
Tyler V <16465776+TerabyteTiger@users.noreply.github.com>
2020-10-12 10:41:45 -0400

    ✨ Layout take up full page height minimum

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -34,3 +34,12 @@ export default {
   },
 };
 </script>
+
+<style>
+  #app {
+    min-height: 100vh;
+    display: flex; 
+    flex-direction: column;
+    justify-content: space-between;
+  }
+</style>
